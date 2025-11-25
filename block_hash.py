ff74fbe6ba9efeefbeb98856ad6762c847993c5c
martynasmat <martynasmat03@gmail.com>
2025-11-26 00:24:33 +0200

    block

--- block_hash.py
+++ block_hash.py
@@ -28,13 +28,14 @@ print(bits)
 print(nonce)
 
 print(
-    f"""
-    version: {version},
-    prev_block_hash: {prev_block_hash},
-    merkle_root: {merkle_root},
-    time: {time},
-    bits: {bits},
-    nonce: {nonce}"""
+f"""
+version: {version},
+prev_block_hash: {prev_block_hash},
+merkle_root: {merkle_root},
+time: {time},
+bits: {bits},
+nonce: {nonce}
+"""
 )
 
 header = (
