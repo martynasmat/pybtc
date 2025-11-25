776735b293601d0f9006537a7f96ec028770b720
martynasmat <martynasmat03@gmail.com>
2025-11-25 18:58:06 +0200

    block

--- block_hash.py
+++ block_hash.py
@@ -1,4 +1,6 @@
 from bitcoin.rpc import RawProxy
+import hashlib as h
+from binascii import unhexlify, hexlify
 
 p = RawProxy()
 blockheight = 277316
@@ -9,17 +11,13 @@ block = p.getblock(blockhash)
 transactions = block['tx']
 block_value = 0
 
-for txid in transactions:
-    tx_value = 0
-    raw_tx = p.getrawtransaction(txid)
-    decoded_tx = p.decoderawtransaction(raw_tx)
+version = block['version']
+prev_block_hash = block['previousblockhash']
+merkle_root = block['merkleroot']
+nonce = block['nonce']
+time = block['time']
+bits = block['difficulty']
 
-    # Iterate through each output in the transaction
-    for output in decoded_tx['vout']:
-
-        # Add up the value of each output
-        tx_value = tx_value + output['value']
-
-        # Add the value of this transaction to the total
-        block_value = block_value + tx_value
-print("Total output value (in BTC) in block #277316: ", block_value)
+header_bin = unhexlify(hex(version) + prev_block_hash + merkle_root + hex(nonce) + hex(time) + hex(bits))
+hash_val = h.sha256(h.sha256(header_bin).digest()).digest()
+print(hexlify(hash_val).decode('utf-8'))
